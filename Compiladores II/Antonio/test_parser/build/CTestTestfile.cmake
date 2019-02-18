3e2493c9ac1c3b6202308119acae0918a27abb1a
Antonio21MP <antonio.mperdomo21@gmail.com>
2019-02-18 15:16:14 -0600

    fix

--- Compiladores II/Antonio/test_parser/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antonio21mp/Documents/compilers_2/proyect/test_parser
-# Build directory: /home/antonio21mp/Documents/compilers_2/proyect/test_parser/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_parser "/home/antonio21mp/Documents/compilers_2/proyect/test_parser/build/test_parser")
